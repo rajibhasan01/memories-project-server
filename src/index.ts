fdb7610660d6926e9f97446c174ad1f2954b023c
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-06-15 19:16:33 +0600

    test case

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express from 'express';
-import registeredRouters from './routes/register-routing-files';
+import express from "express";
+import registeredRouters from "./routes/register-routing-files";
 const app = express();
-const port = process.env.PORT || 5000;// default port to listen
+const port = process.env.PORT || 5000; // default port to listen
 // define a route handler for the default home page
-app.use(express.json());  // support json encoded bodies
-app.use(express.urlencoded({ extended: true})); // support encoded bodies
+app.use(express.json()); // support json encoded bodies
+app.use(express.urlencoded({ extended: true })); // support encoded bodies
 
-app.use(express.static('uploaded-image'))
-app.use('/', registeredRouters);
-
+app.use(express.static("uploaded-image"));
+app.use("/", registeredRouters);
 
 // start the Express server
 app.listen(port, () => {
