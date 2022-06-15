001cf4b210136aa49da71d8f0a7a6f72467424d2
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-06-15 12:52:01 +0600

    updated

--- src/controllers/post/controller.post.ts
+++ src/controllers/post/controller.post.ts
@@ -1,3 +1,4 @@
+import { Post } from "../../model/model.post";
 import { PostService } from "../../services/post/service.post";
 
 const postService = PostService.getInstance();
@@ -10,32 +11,35 @@ export const getPosts = async (req: Request, res: any) => {
         if (result.length > 0) {
           res.status(200).json(result);
         } else {
-          res.status(400).json({ message: "No post found" });
+          res.status(200).json({ message: "No post found" });
         }
       })
       .catch((error) => {
-        res.status(404).json({ message: error.message });
+        res.status(409).json({ message: error.message });
       });
   } catch (error) {
     console.log("Error in add controller get route:", error);
+    res.status(500).json({ message: error.message });
   }
 };
 
-export const createPost = async (req: any, res: any) => {
+export const createPost = async (req:any, res: any) => {
+  const data: Post = req.body;
   try {
     await postService
-      .AddPost(req.body)
+      .AddPost(data)
       .then((result) => {
         if (result) {
-          res.status(200).json(result);
+          res.status(201).json(result);
         } else {
-          res.status(404).json({ message: "can't added the post" });
+          res.status(400).json({ message: "can't added the post" });
         }
       })
       .catch((error) => {
-        res.status(404).json({ message: error.message });
+        res.status(409).json({ message: error.message });
       });
   } catch (error) {
     console.log("Error in add controller post route:", error);
+    res.status(500).json({ message: error.message });
   }
 };
