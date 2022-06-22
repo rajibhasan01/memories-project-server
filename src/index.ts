a1b9be9798e68466e1872f4ec7733e6112040339
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-06-22 17:54:35 +0600

    icrease limit of file upload in express(json())

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import registeredRouters from "./routes/register-routing-files";
 const app = express();
 const port = process.env.PORT || 5000; // default port to listen
 // define a route handler for the default home page
-app.use(express.json()); // support json encoded bodies
-app.use(express.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.json({limit: '50mb'})); // support json encoded bodies
+app.use(express.urlencoded({ extended: true, limit: '50mb' })); // support encoded bodies
 app.use(cors());
 
 app.use(express.static("uploaded-image"));
